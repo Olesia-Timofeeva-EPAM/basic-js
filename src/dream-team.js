600b5f8dfdd6691c67ffcd1a1495f2016e538c45
Olesia Timofeeva <olesia_timofeeva@epam.com>
2020-07-07 14:27:14 +0400

    DreamTeam done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let result = "";
+  if(!Array.isArray(members))
+  return false;
+
+  let firstLetters = members.map(s => { 
+    if(s && typeof s === 'string')
+    {
+    s = s.trim();
+    
+    
+      result = s[0].toUpperCase();
+    return result;
+    }
+    return '';
+  });
+  arr = firstLetters.filter(s => {return s != ''}).sort();
+  return arr.join('');
 };
