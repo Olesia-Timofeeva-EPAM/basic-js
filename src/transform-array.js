7873ffd741872a0303618b4c2c9268e8da1105a4
Olesia Timofeeva <olesia_timofeeva@epam.com>
2020-07-09 02:12:28 +0400

    transform done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr ) {
+  if(!Array.isArray(arr))
+    throw new Exception('Error');
+
+  let result = Array.from(arr);
+  
+  for(let i = 0; i < result.length; i++)
+  {
+    if(typeof result[i] === 'string')
+    {
+      switch(result[i])
+      {
+        case '--discard-next':
+          delete result[i];
+          delete result[i+1];
+          break;
+        case '--discard-prev': 
+        delete result[i];
+        delete result[i-1];
+          break;
+        case '--double-next':
+          if(result[i+1] !== undefined)
+          result[i] = result[i+1];
+          else
+            delete result[i];
+            break;
+        case '--double-prev': 
+        if(result[i-1] !== undefined)
+        result[i] = result[i-1];
+          else
+            delete result[i];
+            break;
+      } 
+    }
+  }
+
+ return result.filter(el => el !== undefined);
 };
